6c24e18d7327f790a8f07ea46a0b28adfdc6362f
mihail-19 <64752039+mihail-19@users.noreply.github.com>
2026-05-19 21:12:21 +0300

    menu burger

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
+gsap.registerPlugin(ScrollTrigger);
 export default function Content() {
- 
+ useEffect(() => {
+  gsap.fromTo(
+  ".fade__header",
+  {
+    opacity: 0,
+    y: 40,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".fade__header",
+      start: "top 80%",
+      end: "top 40%",
+      scrub: true,
+    },
+  }
+);
+ gsap.to(".fade__header", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".fade__header",
+      start: "center center",
+      end: "bottom top",
+      scrub: true,
+    },
+  });
+}, []);
 
   return (
     <main className="content">
       <section className="block">
-        <h1>Відпочинок у Коропово </h1>
+        <h1 className="fade__header">Відпочинок у Коропово </h1>
         <div className="order">
             <div className="order__section">
                 <p>Дата заселення</p>
--- src/Headr.jsx
+++ src/Headr.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import menuIcon from "/menu.png"
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false)
+
+
   return (
     <header className="hdr">
       <div>KOROPOVO</div>
-      <div>Menu</div>
+      <div className="header__menu-icon" onClick={() => setShowMenu(true)}></div>
+      {showMenu && (
+        <div className="header__menu-body">
+
+          <div className="header__menu-top">
+            <div className="header__menu-close" onClick={() => setShowMenu(false)}></div>
+          </div>
+
+        </div>
+      )}
+      
            
     </header>
   );
